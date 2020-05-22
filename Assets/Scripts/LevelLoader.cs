2a7567c703850b7c0cd6fbc737e82ad863574e9f
Mikołaj Nawrocki <m.nawrocki793@gmail.com>
2020-05-22 12:07:02 +0200

    PP03_P1_UKOŃCZONO_DWIE_PIERWSZE_SCENY
    + Welcome Screen, Start Screen, dźwięk powitalny, ładowanie kolejnej sceny

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int waitInSeconds = 4;
+    int currentSceneIndex;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(waitInSeconds);
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
