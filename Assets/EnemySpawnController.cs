80e9585d04ec54a16bb429be5ef09c0cbac4e95c
Mikołaj Nawrocki <m.nawrocki793@gmail.com>
2020-05-24 11:54:03 +0200

    PP03_P3_UKOŃCZONO_SPAWNOWANIE_PRZECIWNIKÓW
    + animacje postaci, ruch przeciwników, spawn przeciwnika

---
+++ Assets/EnemySpawnController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnController : MonoBehaviour
+{
+    [SerializeField] float minSpawnTime = 1f;
+    [SerializeField] float maxSpawnTime = 1f;
+    [SerializeField] EnemyController enemyPrefab;
+
+    bool spawn = true;
+
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minSpawnTime, maxSpawnTime));
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    [Range(0f, 5f)] [SerializeField] float walkSpeed = 1f;
+    private void Update()
+    {
+        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+    }
+}
